3dec28a55034ac827697000005843a44ba39e9dd
RAGHAVENDRA S.H <raghavendrahande@gmail.com>
2024-12-02 18:01:27 +0530

    Initial API

---
+++ F1flaskAPI.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Route: Home
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "round": "R24.ABU DHABI",
+        "date":"06-08",
+        "month":"DEC",
+        "flag":"🇦🇪",
+        "fp1time":"03:00 PM",
+        "fp2time":"06:30 PM",
+        "fp3time":"04:00 PM",
+        "q1time":"07:30 PM",
+        "racetime":"06:30 PM",
+        "imglink":"https://media.formula1.com/image/upload/f_auto,c_limit,w_1440,q_auto/f_auto/q_auto/content/dam/fom-website/2018-redesign-assets/Track%20icons%204x3/Abu%20Dhab%20carbon"
+    })
+
+if __name__ == "__main__":
+    app.run()
